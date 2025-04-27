49370ae4cc950bfcae18a272f1998029debcc599
Caio Andres <caioandresgit@outlook.com>
2025-04-27 20:02:53 -0300

    fix: class uncommented in 148. sort list problem.

--- problems/solutions/0148-sort-list/solution.py
+++ problems/solutions/0148-sort-list/solution.py
@@ -1,3 +1,10 @@
+from typing import Optional
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Base case: if the list is empty or has only one node, it's already sorted
